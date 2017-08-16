851e0f66c8c59ab0d27410d2a5ba225109aff304
carllx <carllx@hotmail.com>
2017-08-16 12:43:03 +0200

    尝试修复git log中文现实问题

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ import {ui}  from '../utils/ui'
 
 css.global(
 
-  'html, body', { 
+  'html, body', {
     fontSize: '100%',
     color:ui.color.secondary_on_light,
   },
@@ -31,10 +31,10 @@ css.global(
 
 
 export default class extends Component {
-  
+
   constructor (props) {
       super(props)
-      this.state = { 
+      this.state = {
         /*
         device   : desktop / moblie / tablet    --2.
         direction: portrait / landscape
@@ -51,7 +51,7 @@ export default class extends Component {
        }
        this.onScorll = debounce(this.handleScroll ,500 );
 
-       
+
     }
 
   static async getInitialProps () {
@@ -71,26 +71,26 @@ export default class extends Component {
     const ScrollY = window.scrollY;
     //  ↑ or ↓ ???
     const isScrollUp = ( ScrollY - this.prevScrollY)<=0 ;
-    
+
     if(isScrollUp) {
       console.log('↑');
-      
+
     }else{
       console.log('↓');
     }
     // 刷新当前scroll所在位置
     this.prevScrollY = ScrollY;
-        
+
     }
-    
+
 
     componentWillMount(){
 
       if (typeof window == 'undefined') {return};
 
-      
 
-      
+
+
 
 
 
@@ -100,12 +100,12 @@ export default class extends Component {
       window.removeEventListener('scroll', this.onScorll, false);
 
     }
-    
+
 
 
   componentDidMount(){
 
-    
+
 
     // SCROLL
     this.prevScrollY = window.scrollY;
@@ -115,7 +115,7 @@ export default class extends Component {
     // 检测移动硬件
     if (typeof navigator !== 'undefined') {
 
-      // device 
+      // device
       if(isMobile()==true){//isMobile
         this.setState({device : 'isMobile'})
       }else if (isTablet()==true){
@@ -142,9 +142,9 @@ export default class extends Component {
                   '\n高度:',window.innerHeight,
                   '\n宽度:',window.innerWidth
                   );
-        
+
   }
-  
+
 
   render () {
     return (
@@ -168,20 +168,20 @@ export default class extends Component {
             <h3>Z + AI</h3>
             <p>{`
               ZAI is alive, 每个网页元素 (例如"按钮,图片卡,导航栏,Logo"), 分别视为有生命的独立元素. 它们不只是存在, 更是活着的.\n
-              Z + AI 的实现思路是为元素注入AI特性,强化每个元素各自的个性,改进与用户交互的反应. 
+              Z + AI 的实现思路是为元素注入AI特性,强化每个元素各自的个性,改进与用户交互的反应.
               \r\n该特性是我们的长期目标,将在网站第二期工程由ZAI团队共同设计.`}</p>
-            
+
             <h3>NULL DESIGN</h3>
             <p>{`纸张的空能召唤创造者的灵感,当它载满灵感的时候也是他最有价值的, 因此我们网站设计将特别关注空白的地方`}</p>
 
             <h3>感应式布局</h3>
-            <p>{`我们关心你会在什么场合,使用什么新设备访问我们, 不论你们在使用电脑 / 手机 / 平板 / 网络TV, 
+            <p>{`我们关心你会在什么场合,使用什么新设备访问我们, 不论你们在使用电脑 / 手机 / 平板 / 网络TV,
               或者你的设备90度/180度,我们都会考虑到,我们会分别为你定义不一样的网页布局
               `}</p>
-            
+
             <h3>流量节约</h3>
-            <p>{`我们知道你们的访问是有代价, 所以我们在设计一套流量节制系统, 
-              例如当你的访问涉及图片内容时,我们会先根据你的设备先定制合适的像素,再发送到你的设备上. 
+            <p>{`我们知道你们的访问是有代价, 所以我们在设计一套流量节制系统,
+              例如当你的访问涉及图片内容时,我们会先根据你的设备先定制合适的像素,再发送到你的设备上.
               你屏幕以外的图片,我们是不会让它占用你们的流量,尽管它就在一个页面内,
               它们只有在你需要的时候我们才会传送给你.`}</p>
             <h3>硬件检测</h3>
@@ -189,9 +189,9 @@ export default class extends Component {
             <span>识别你的设备是 <glamorous.Span color='#d14'>{ /*this.state.device*/}</glamorous.Span></span>
             <h3>ZAI further away(渐行渐远)</h3>
             <p>{`我们非常在乎你未来的浏览方式,对已经成为主流的技术/方式, 我们会不惜进行改造,就如我们不会为求选用已经发展成熟的
-              wordpress框架制作我们的网站,尽管它更简单和更有效率. 我们分析未来趋向,不惜花费大量的学习/排错成本, 
+              wordpress框架制作我们的网站,尽管它更简单和更有效率. 我们分析未来趋向,不惜花费大量的学习/排错成本,
               探索2017+最新的框架和开发语言, 所以我们可以不断为你提供最新的服务和更多的可能性`}</p>
-            
+
             <h2>Change Log 网站进度</h2>
             <h3>2017-8-8</h3>
             <p>引入device.js 硬件检测脚本</p>
@@ -224,6 +224,9 @@ export default class extends Component {
 
             <h3>2017-8-15</h3>
             <p>试用构建图片自动裁剪系统,于服务器中</p>
+
+            <h3>2017-8-16</h3>
+            <p>尝试修复git log中文现实问题</p>
             <p></p>
           </glamorous.Div>
         </content>
