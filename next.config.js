fbf987649adf0b3d74607ea98afea9c29a9d261d
chengcestudio <chengcestudio@gmail.com>
2017-12-17 23:39:20 +0100

    next.config 似乎无效， 百度文件验证 ， artist 全部递交

--- next.config.js
+++ next.config.js
@@ -12,39 +12,7 @@ module.exports = {
     cp('./static/sitemap.xml','./out/sitemap.xml');
     cp('./static/baidu_verify_BWMAZPDlTw','./out/baidu_verify_BWMAZPDlTw');
 
-    /*
-    // we fetch our list of posts, this allow us to dynamically generate the exported pages
-    const response = await fetch('http://jsonplaceholder.typicode.com/posts?_page=1')
-    const postList = await response.json()
-    */
 
-    /*{
-      "userId": 1,
-      "id": 1,
-      "title": "...",
-      "body": "...."
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "...",
-      "body": "...."
-    },*/
-
-
-    // console.log('postList: ',postList)
-    // console.log('artisti: ',artisti)
-    // tranform the list of posts into a map of pages with the pathname `/post/:id`
-    // const pages = postList.reduce(
-    //   (pages, post) =>
-    //     Object.assign({}, pages, {
-    //       [`/post/${post.id}`]: {
-    //         page: '/post',
-    //         query: { id: post.id }
-    //       }
-    //     }),
-    //   {},
-    // )
 
     // tranform the list of posts into a map of pages with the pathname `/post/:id`
 
