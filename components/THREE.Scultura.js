80523e2abd63e5d5f228c3c24ad5039e751bd09a
chengcestudio <chengcestudio@gmail.com>
2018-01-15 00:51:06 +0100

    ⛄️ 尝试 facebook 社交链接,弹出新的tab访问

---
+++ components/THREE.Scultura.js
@@ -0,0 +1,146 @@
+// @ wait react-vr
+import {TimelineMax} from "gsap";
+import * as THREE from 'three';
+
+// import  {PureComponent} from 'react'
+
+// import { bindActionCreators } from 'redux'
+
+// import { connect } from 'react-redux'
+
+
+// export default class Z3D_SPHERE extends PureComponent {
+//     constructor(props){
+//         super(props);
+//         // 球面的渲染参数
+//         this.opacity = 1;
+//         this.polygons = 15;
+//         this.rotation = true;
+//         this.duration = 5;
+//         this.direction = 2;
+//     }
+
+//     render(){
+//         return null
+//     }
+// }
+// let store = initStore()  读取state
+// const {vw,vh}=store.getState().Root.view_size
+
+// var vw = window.innerWidth,
+//     vh = window.innerHeight;
+// var vw = 800,
+//     vh = 800;
+let renderer, scene, camera, allTriangles;
+
+
+
+const ctrl = {
+    opacity : 1,
+    polygons : 15,
+    rotation : true,
+    duration : 5,
+    direction : 2,
+}
+
+
+
+export const ThreeInit = function () {
+    /* INIT */
+    var vh = document.documentElement.clientHeight,
+        vw = document.documentElement.clientWidth;
+
+    renderer = new THREE.WebGLRenderer({
+        canvas: document.getElementById('scene'),
+        antialias: true
+    });
+    renderer.setSize(vw, vh);
+    renderer.setClearColor( 0xffffff);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(50, vw / vh, 0.1, 10000);
+    camera.position.set(0, 0, 800);
+    scene.add(camera);
+
+    init_Triangles();
+
+    render();
+}
+
+
+const init_Triangles = function() {
+
+    if (allTriangles) {
+        scene.remove(allTriangles);
+    }
+    allTriangles = new THREE.Object3D();
+    scene.add(allTriangles);
+    const geometry = new THREE.SphereGeometry(1500, 15, 7);
+    for (var i = 0, j = geometry.faces.length; i < j; i++) {
+        //Get one face from the sphere
+        var whichTriangle = i;
+        if (ctrl.direction == 1) {
+            whichTriangle = (j - 1) - i;
+        } else if (ctrl.direction == 2) {
+            whichTriangle = i % 2 === 0 ? i : j - i;
+        }
+        var face = geometry.faces[whichTriangle];
+
+        //Create a geometry for that face
+        var geometry2 = new THREE.Geometry();
+
+        //Add the vertices of the face in the geometry
+        geometry2.vertices = face.vertexNormals;
+
+        //Set a new face
+        geometry2.faces.push(new THREE.Face3(0, 1, 2));
+
+        //Create a custom material
+        var material = new THREE.MeshBasicMaterial({
+            side: THREE.DoubleSide,
+            wireframe: false,
+            transparent: true,
+            opacity: ctrl.opacity
+        });
+
+        //Create a Mesh object
+        var triangle = new THREE.Mesh(geometry2, material);
+        var randomColor = 0.6+Math.random()*0.4;
+        triangle.material.color = new THREE.Color(randomColor, randomColor, randomColor);
+        allTriangles.add(triangle);
+        // triangle.scale
+        triangle.tl = new TimelineMax({
+            repeat: -1,
+            repeatDelay: (j - i) / (j * 4),
+            delay: i / (ctrl.polygons * 5),
+            yoyo: true
+        });
+
+        const radm = Math.random()
+        triangle.scale.set(900,900,900)
+        triangle.tl
+            .to(triangle.scale, 50, {
+                x: 900 + radm*3000,
+                y: 900 + radm*3000,
+                z: 900 + radm*3000,
+                ease: Linear.ease,//Back.easeIn, Back.easeOut, Bounce.easeIn, Bounce.easeOut, Linear.ease, Elastic.easeOut
+            })
+
+    }
+
+
+};
+
+var render = function() {
+    requestAnimationFrame(render);
+
+    if (ctrl.rotation) {
+        allTriangles.rotation.y += 0.0005;
+    }
+
+    renderer.render(scene, camera);
+};
+
+
+
---
+++ pages/demo3.js
@@ -0,0 +1,161 @@
+import { PureComponent } from 'react'
+import { bindActionCreators } from 'redux'
+import withRedux from 'next-redux-wrapper'
+
+import { css  } from 'glamor'
+import {initStore} from '~/store'
+import {setScroll,switchLanguage,setViewSize,onDevice ,setBrowser} from'~/reducers/root'
+import { setPanelOn } from '~/reducers/nav'
+
+import NoSSR from 'react-no-ssr';
+import Head from 'next/head'
+import SEO from '~/components/SEO.index'
+import Nav from '~/components/nav'
+import About from '~/components/about'
+
+import Resizer from '~/components/controller.resize'
+import {ThreeInit} from'~/components/THREE.Scultura.js'
+
+import {isMobile  ,isTablet ,isLandscape, getLanguer}  from '../utils/device'
+
+import {ui ,GR }  from '../utils/ui'
+
+
+
+
+
+class Index extends PureComponent {
+  static getInitialProps ({isServer}) {
+    return Object.assign({},{isServer})
+  }
+
+  constructor (props) {
+      super(props)
+    }
+
+
+  setLanguage=(language)=>{
+    this.props.switchLanguage(language)
+  }
+
+  setDevice=()=>{
+      let whatDevice ;
+      if(isMobile()) {whatDevice = 'mobile'}
+      else if(isTablet()) {whatDevice = 'tablet'}
+      else {whatDevice = 'desktop'}
+      this.props.onDevice(whatDevice)
+  }
+
+
+
+  componentDidMount(){
+
+    // DEVICE
+    this.setDevice()
+
+    /* LANGUGE */
+    this.setLanguage(getLanguer())
+
+    /* height width DIRECTION */
+    // this.setViewSize()
+
+    ThreeInit();//@bug 不能resize,不能交互
+  }
+
+
+
+  render () {
+    // debugger
+    const language = this.props.language ||'zh'
+    const {vw,vh,is_landscape} = this.props.view_size||{view_size:{vw:0,vh:0,is_landscap:false}}
+
+    return (
+      <main >
+        <Head>
+          <title>{'中艺国际 - ZAI - zhong art'}</title>
+          <meta content={`中艺国际, ZAI, zhongart international`} name='title' />
+          <meta content={`中艺国际, ZAI, zhongart international`} property='og:title' />
+          <meta content={`中艺国际, ZAI, zhong art international, 中艺国际是一个位于意大利佛罗伦萨的国际性艺术与文化交流机构。成立于2017年，在中国北京和意大利佛罗伦萨均有办事机构，我们的宗旨是为中国和意大利两国提供文化领域的交流与合作，通过积极为两国的艺术机构之间和艺术家之间策划展览和艺术家之间的学术交流活动，推动促进两国文化积极友好的发展。我们与意大利很多国家级的很多重要文化机构如文化部、大使馆等都有长期项目往来，与一些艺术类高等院校如罗马美术学院、佛罗伦萨大学美术学院、欧洲设计学院等都有校际合作关系。我们为专业人士提供专业性对接交流，旨在为中国和意大利两国的艺术发展建立文化沟通的桥梁。`} name='description' />
+          <meta content={`中艺国际, ZAI, zhong art international,中艺国际是一个位于意大利佛罗伦萨的国际性艺术与文化交流机构。成立于2017年，在中国北京和意大利佛罗伦萨均有办事机构，我们的宗旨是为中国和意大利两国提供文化领域的交流与合作，通过积极为两国的艺术机构之间和艺术家之间策划展览和艺术家之间的学术交流活动，推动促进两国文化积极友好的发展。我们与意大利很多国家级的很多重要文化机构如文化部、大使馆等都有长期项目往来，与一些艺术类高等院校如罗马美术学院、佛罗伦萨大学美术学院、欧洲设计学院等都有校际合作关系。我们为专业人士提供专业性对接交流，旨在为中国和意大利两国的艺术发展建立文化沟通的桥梁。`} property='og:description' />
+          <meta content={`中艺国际, ZAI, zhongart, zhong art international, 意大利, 佛罗伦萨, firenze, arte, Gallery`} name='keywords'/>
+          <meta http-equiv="keyword" content="中艺国际, ZAI, zhongart, zhong art international, 意大利, 佛罗伦萨, firenze, arte, Gallery" />
+            {/*name="twitter:site" content="@artstationhq"
+            name="twitter:site_name" content="ArtStation"
+            name="twitter:title" content="Fenghua Zhong"
+            name="twitter:card" content="summary"
+            name="twitter:description" content="concept artist E-art School of Design"
+            name="twitter:image"*/}
+        </Head>
+
+        {/*NAV*/}
+        <NoSSR>
+          <Nav
+           // vw = {vw}
+           // vh = {vh}
+           // isLandscape={isLandscape}
+           // language= {language}
+           // marginW = {GR.vw(5)}
+           show_on_init = {false}//默认关闭
+          />
+        </NoSSR>
+
+        {/*服务器初始内容*/}
+        <NoSSR onSSR={<SEO/>}>
+          <ABS>{'客户端'}</ABS>
+        </NoSSR>
+
+        {/*THREE*/}
+          <canvas
+        {...css({position:'absolute',top:0,left:0,cursor:'pointer',})}
+        id = "scene" width={vw} height={vh}
+        onClick = {()=>{this.props.setPanelOn('show')}}
+        />
+
+
+        {/*INFO_LOGO*/}
+        <About></About>
+
+        <Resizer/>
+
+      </main>
+
+
+
+    )
+  }
+
+}
+
+const mapStateToProps = (state) => ({
+    view_size:state.Root.view_size,
+    os:state.Root.os,
+    device:state.Root.device,
+    language:state.Root.language,
+    // nav:state.nav
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // root
+    switchLanguage: bindActionCreators(switchLanguage, dispatch),
+    // setBrowser: bindActionCreators(setBrowser, dispatch),
+    onDevice: bindActionCreators(onDevice, dispatch),
+    //nav
+    setPanelOn:bindActionCreators(setPanelOn, dispatch )
+  }
+}
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Index)
+
+
+
+const ABS = (props)=>
+   <div
+    {...css({
+         position:'absolute',
+         zIndex:-1,
+    })}
+    >
+        {props.children}
+    </div>
+
